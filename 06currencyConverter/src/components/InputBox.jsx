6eae909600c3bd38d2c96dd2c09943dd2850b520
Sahil Lodhi <sahillodhi369@gmail.com>
2026-01-06 13:59:07 +0530

    complete the unfinished currency project

--- 06currencyConverter/src/components/InputBox.jsx
+++ 06currencyConverter/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useId} from 'react'
 
 function InputBox({
     label,
@@ -14,14 +14,16 @@ function InputBox({
 }) {
    
 
+    const amountInputId = useId()
+
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex  ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -35,7 +37,7 @@ function InputBox({
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
                     value={selectCurrency}
-                    onChange={(e)=>onCurrencyChange && oncuechange(e.target.value)}
+                    onChange={(e)=>onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
                     
                 >
