0096afaa4b0b611a4b21e27daf8bad794e1d6e1e
Sahil Lodhi <sahillodhi369@gmail.com>
2026-02-04 19:55:46 +0530

    redux crash course

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import './index.css'
+
+function App() {
+
+
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
+
+
