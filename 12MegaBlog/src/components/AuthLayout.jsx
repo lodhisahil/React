af3f51b3ea94bc604520dd05c3e492022260a798
Sahil Lodhi <sahillodhi369@gmail.com>
2026-02-09 13:01:21 +0530

    MegaBlog- use React hook form

---
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+export default function Protected({
+    children, 
+    authentication = true
+}) {
+    const navigate = useNavigate()
+    const [loader , setLoader] = useState(true)
+
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+
+        // TOdo : make it easy to understand    
+        // if(authentication === true){
+        //     navigate("/")
+        // } else if (authStatus === false){
+        //     navigate("/login")
+        // }
+        // let authValue = authStatus === true ? true : false
+
+        if (authentication && authStatus !== authentication) {
+            navigate("/login")
+        } else if (!authentication && authStatus !== authentication){
+            navigate("/")
+        }
+        setLoader(false)
+    } , [authStatus, navigate, authentication])
+  return loader ? <h1>Loading....</h1> : <>{children}</>
+}
+
